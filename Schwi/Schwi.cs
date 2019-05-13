3149ec99e674edb2a05f2039af85677ff1e7766b
Darren Ling <pantalaimonpan@gmail.com>
2019-05-13 16:00:02 -0400

    Async Context

---
+++ Schwi/Schwi.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Schwi
+{
+    public class Schwi
+    {
+        public static void Main(string[] args)
+            => new Schwi().MainAsync().GetAwaiter().GetResult();
+        public async Task MainAsync()
+        {
+
+        }
+    }
+}
